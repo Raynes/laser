2f5669f0825a44cb28b06858274c93c80d08a8f8
Anthony Grimes <i@raynes.me>
2012-12-13 22:32:23 -0600

    Don't forget teh tinsels.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject me.raynes/laser "0.1.0"
-  :description "An HTML transformation library similar to Enlive"
+  :description "An HTML transformation library similar to Enlive/Tinsel."
   :url "https://github.com/Raynes/laser"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
