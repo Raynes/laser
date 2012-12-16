bb04e1dcf8959956ae29805ad028ea3441a20118
Anthony Grimes <i@raynes.me>
2012-12-15 21:34:43 -0600

    Upgrade hickory to fix all my problems.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject me.raynes/laser "0.1.3"
+(defproject me.raynes/laser "0.1.4"
   :description "An HTML transformation library similar to Enlive/Tinsel."
   :url "https://github.com/Raynes/laser"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [hickory "0.2.0"]])
+                 [hickory "0.2.1"]])
