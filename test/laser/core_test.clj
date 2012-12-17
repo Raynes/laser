7ee2150523bb29ae1ca4cc045aa3064bb2337421
Anthony Grimes <i@raynes.me>
2012-12-17 06:29:33 -0600

    Add some tests (nowhere near full coverage yet).

--- test/laser/core_test.clj
+++ test/laser/core_test.clj
@@ -1,7 +1,47 @@
 (ns laser.core-test
-  (:use clojure.test
-        laser.core))
+  (:require [clojure.test :refer :all]
+            [me.raynes.laser :as l]
+            [clojure.zip :as zip]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;; Selectors
+
+(def html [{:type :element, :attrs {:class "a b c", :id "hi"}, :tag :p, :content ["hi"]}])
+
+(deftest element=-test
+  (is (true? ((l/element= :p) html)))
+  (is (false? ((l/element= :pre) html))))
+
+(deftest attr=-test
+  (is (true? ((l/attr= :class "a b c") html)))
+  (is (false? ((l/attr= :foo "bar") html))))
+
+(deftest attr?-test
+  (is (true? ((l/attr? :class) html)))
+  (is (false? ((l/attr? :foo) html))))
+
+(deftest class=-test
+  (is (false? ((l/class= "a") html)))
+  (is (true? ((l/class= "a" "b" "c") html)))
+  (is (false? ((l/class= "d") html))))
+
+(deftest id=-test
+  (is (true? ((l/id= "hi") html)))
+  (is (false? ((l/id= "bye") html))))
+
+(deftest any-test
+  (is (true? ((l/any) html))))
+
+;; Combinators
+
+(deftest negate-test
+  (is (false? ((l/negate (l/element= :p)) html))
+      (true? ((l/negate (l/element= :pre)) html))))
+
+(deftest select-and-test
+  (is (true? ((l/select-and (l/element= :p) (l/attr? :class)) html)))
+  (is (false? ((l/select-and (l/element= :pre) (l/attr? :class)) html))))
+
+(deftest select-or-test
+  (is (true? ((l/select-or (l/element= :p) (l/attr? :class)) html)))
+  (is (true? ((l/select-or (l/element= :pre) (l/attr? :class)) html)))
+  (is (false? ((l/select-or (l/element= :pre) (l/attr? :foo)) html))))
