4974b4bbabce3d698c2f90fed705e5224b561d75
Anthony Grimes <i@raynes.me>
2012-12-17 05:47:37 -0600

    Inc version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject me.raynes/laser "0.1.5"
+(defproject me.raynes/laser "0.1.6"
   :description "An HTML transformation library similar to Enlive/Tinsel."
   :url "https://github.com/Raynes/laser"
   :license {:name "Eclipse Public License"
