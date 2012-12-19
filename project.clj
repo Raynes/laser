53d707e8a2761b3446f8da6fdcdc5441ecb972df
Anthony Grimes <i@raynes.me>
2012-12-18 19:11:02 -0600

    Added codox stuff.

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [hickory "0.2.1"]])
+                 [hickory "0.2.1"]]
+  :codox {:src-dir-uri "https://github.com/Raynes/laser/tree/e1beb765cf40564a789fa5d2d5f795e9df724530"
+          :src-linenum-anchor-prefix "L"})
