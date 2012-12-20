b2a4d29e350433f54bcdb0acfe226654f3adf1c8
Anthony Grimes <i@raynes.me>
2012-12-20 04:20:54 -0600

    Upgrade hickory.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject me.raynes/laser "0.1.6"
+(defproject me.raynes/laser "0.1.7"
   :description "An HTML transformation library similar to Enlive/Tinsel."
   :url "https://github.com/Raynes/laser"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [hickory "0.2.1"]]
+                 [hickory "0.2.2"]]
   :codox {:src-dir-uri "https://github.com/Raynes/laser/tree/e1beb765cf40564a789fa5d2d5f795e9df724530"
           :src-linenum-anchor-prefix "L"})
