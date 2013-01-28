4e6f1352981588866231a0f87cbf8513150c72cc
Anthony Grimes <i@raynes.me>
2013-01-28 04:45:49 -0600

    Inc version.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject me.raynes/laser "0.1.20"
+(defproject me.raynes/laser "0.1.21"
   :description "An HTML transformation library similar to Enlive/Tinsel."
   :url "https://github.com/Raynes/laser"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [hickory "0.2.2"]]
+                 [hickory "0.2.3"]]
   :profiles {:dev {:dependencies [[midje "1.5-alpha8"]]}}
   :plugins [[lein-midje "3.0-alpha4"]]
   :codox {:src-dir-uri "https://github.com/Raynes/laser/tree/e1beb765cf40564a789fa5d2d5f795e9df724530"
